ad0cfcb1538e7d2dba9abcf2d603651732a8d538
Patrick Godau <patrick.scholz@dkfz-heidelberg.de>
2025-05-23 09:33:18 +0200

    [PLUGIN] makes runner and reqs importable from top level of the LSF plugin

--- plugins/lsf/src/mml_lsf/__init__.py
+++ plugins/lsf/src/mml_lsf/__init__.py
@@ -3,6 +3,10 @@
 # SPDX-FileCopyrightText: Copyright 2024 German Cancer Research Center (DKFZ) and contributors.
 # SPDX-License-Identifier: MIT
 #
+
+from mml_lsf.requirements import LSFSubmissionRequirements
+from mml_lsf.runner import LSFJobRunner
+
 VERSION = (0, 5, 1)
 __version__ = ".".join(map(str, VERSION))
-__all__ = ["__version__"]
+__all__ = ["__version__", "LSFJobRunner", "LSFSubmissionRequirements"]
